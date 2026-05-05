e90a81c68285ce08729d50aea70cc35e8c8143c7
ahmoin <123082059+ahmoin@users.noreply.github.com>
2026-05-04 21:03:08 -0400

    feat: add dashboard

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,15 @@
-import { betterAuth } from 'better-auth';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
-import { db } from '@/lib/db';
-import * as schema from '@/lib/schema';
+import { dash } from "@better-auth/infra";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/lib/db";
+import * as schema from "@/lib/schema";
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
-		provider: 'pg',
+		provider: "pg",
 		schema,
 	}),
+	plugins: [dash()],
 	baseURL: process.env.BETTER_AUTH_URL ?? "http://localhost:3000",
 	trustedOrigins: ["http://localhost:3000", "https://zomath.vercel.app"],
 	emailAndPassword: {
